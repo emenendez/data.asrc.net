89efc971727b94ca426e704d018ef2a6087e891c
Eric Menendez <ericmenendez@gmail.com>
2013-11-17 18:48:18 -0500

    Add JSON encoders/decoders to Team model

--- app/models/Team.php
+++ app/models/Team.php
@@ -2,6 +2,16 @@
 
 class Team extends Eloquent {
 	protected $hidden = array('api_key', 'private_key');
+
+	public function getPrimaryAreaAttribute($value)
+	{
+		return json_decode($value);
+	}
+
+	public function setPrimaryAreaAttribute($value)
+	{
+		$this->attributes['primary_area'] = json_encode($value);
+	}
 }
 
 ?>
