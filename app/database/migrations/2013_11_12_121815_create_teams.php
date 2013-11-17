e94a9cc0eea5839f6f3a9c4230d616c97125da2a
Eric Menendez <ericmenendez@gmail.com>
2013-11-17 14:50:48 -0500

    Create teams table and model

---
+++ app/database/migrations/2013_11_12_121815_create_teams.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeams extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		// Create teams table
+		Schema::create('teams', function($table){
+			$table->increments('id');
+			$table->string('name');
+			$table->string('acronym');
+			$table->string('address');
+			$table->string('address2');
+			$table->string('city');
+			$table->string('state', 2);
+			$table->string('zip', 5);
+			$table->text('primary_area');
+			$table->string('phone');
+			$table->string('email');
+			$table->string('api_key', 32);
+			$table->binary('private_key');
+			$table->binary('public_key');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		// Drop teams table
+		Schema::drop('teams');
+	}
+
+}
---
+++ app/models/Team.php
@@ -0,0 +1,7 @@
+<?php
+
+class Team extends Eloquent {
+	protected $hidden = array('api_key', 'private_key');
+}
+
+?>
