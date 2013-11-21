809d89a01b08fe0fea33be94664ca43dde361067
Eric Menendez <ericmenendez@gmail.com>
2013-11-21 17:50:40 -0500

    Revert "Add CustomTypes to service providers"
    This reverts commit 66b0c6c6f118e9d515e67cbf61cfa4107c7b457b.

--- app/config/app.php
+++ app/config/app.php
@@ -114,7 +114,6 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Way\Generators\GeneratorsServiceProvider',
-		'Emenendez\CustomTypes\CustomTypesServiceProvider',
 
 	),
 
