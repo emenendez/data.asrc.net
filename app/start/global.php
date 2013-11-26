002fbd95cd743a8fc3a33cb6e890af38e7993f19
Eric Menendez <ericmenendez@gmail.com>
2013-11-25 23:32:47 -0500

    First attempt at error handling

--- app/start/global.php
+++ app/start/global.php
@@ -48,10 +48,7 @@ Log::useDailyFiles(storage_path().'/logs/'.$logFile);
 |
 */
 
-App::error(function(Exception $exception, $code)
-{
-	Log::error($exception);
-});
+require app_path().'/errors.php';
 
 /*
 |--------------------------------------------------------------------------
