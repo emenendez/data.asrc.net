be1d9f1d72074bb8095ac5357dcf35947c0d04a8
Eric Menendez <ericmenendez@gmail.com>
2013-11-26 19:53:58 -0500

    Fix error handling

--- app/errors.php
+++ app/errors.php
@@ -37,33 +37,3 @@ App::error(function(Symfony\Component\HttpKernel\Exception\HttpException $e, $co
                 'error' => $e->getMessage() ?: $default_message,
         ), $code, $headers);
 });
-
-// ErrorMessageException handler
-App::error(function(ErrorMessageException $e)
-{
-        $messages = $e->getMessages()->all();
-
-        return Response::json(array(
-                'error' => $messages[0],
-        ), 400);
-});
-
-// NotFoundException handler
-App::error(function(NotFoundException $e)
-{
-        $default_message = 'The requested resource was not found';
-
-        return Response::json(array(
-                'error' => $e->getMessage() ?: $default_message,
-        ), 404);
-});
-
-// PermissionException handler
-App::error(function(PermissionException $e)
-{
-        $default_message = 'Insufficient privileges to perform this action';
-
-        return Response::json(array(
-                'error' => $e->getMessage() ?: $default_message,
-        ), 403);
-});
